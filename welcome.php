aaa27eb65cb1c8c93be4fa476f9f49ff61163a92
Mika Madrigal <mikamadrigal10@gmail.com>
2026-04-19 16:26:38 +0800

    Added welcomepage branch and background image

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <style>
+        body { 
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+            background-image: url(bggg.jpg);
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+            min-height: 100vh;   
+            margin: 0;
+        }
